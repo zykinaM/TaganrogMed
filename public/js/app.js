cff8afeca72fb7b1a4f7f285d0671c416c9dea6f
Марина Зыкина < >
2016-05-11 22:42:59 +0300

    commit

---
+++ public/js/app.js
@@ -0,0 +1,48 @@
+'use strict';
+
+// Declare app level module which depends on filters, and services
+angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
+  config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: 'partials/index',
+        controller: IndexCtrl
+      }).
+      when('/addPost', {
+        templateUrl: 'partials/addPost',
+        controller: AddPostCtrl
+      }).
+      when('/readPost/:id', {
+        templateUrl: 'partials/readPost',
+        controller: ReadPostCtrl
+      }).
+      when('/editPost/:id', {
+        templateUrl: 'partials/editPost',
+        controller: EditPostCtrl
+      }).
+      when('/deletePost/:id', {
+        templateUrl: 'partials/deletePost',
+        controller: DeletePostCtrl
+      }).
+
+
+      when('/search', {
+        templateUrl: 'search'
+      }).
+      when('/auth_clinic', {
+        templateUrl: 'auth_clinic'
+      }).
+      when('/registr_clinic', {
+        templateUrl: 'registr_clinic'
+      }).
+      when('/index_clinic', {
+        templateUrl: 'index_clinic'
+      }).
+
+
+      
+      otherwise({
+        redirectTo: '/'
+      });
+    $locationProvider.html5Mode(true);
+  }]);
---
+++ routes/index.js
@@ -0,0 +1,29 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  res.render('index');
+};
+
+exports.search = function(req, res){
+  res.render('search');
+};
+
+exports.auth_clinic = function(req, res){
+  res.render('auth_clinic');
+};
+
+exports.registr_clinic = function(req, res){
+  res.render('registr_clinic');
+};
+
+exports.index_clinic = function(req, res){
+  res.render('index_clinic');
+};
+
+exports.partials = function (req, res) {
+  var name = req.params.name;
+  res.render('partials/' + name);
+};
